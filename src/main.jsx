14fa01b9db6ee18272b07c075772bf1bc2d13406
seu-username <alexandresilva.02101978@gmail.com>
2025-10-22 16:27:14 -0300

    Mudanças

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes.jsx'
+import { GlobalStyles } from './styles/GlobalStyles.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} basename="/Projeto-15.Cadastro-de-usuarios" />
+    <GlobalStyles />
   </React.StrictMode>,
 )
